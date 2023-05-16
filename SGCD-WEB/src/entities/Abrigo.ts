e2284b6e2e8b4fabef3cef8eb370440ba44bbd2a
Thiago Calmon <thiago.calmon4@gmail.com>
2023-05-16 08:13:26 -0300

    alter database

--- SGCD-WEB/src/entities/Abrigo.ts
+++ SGCD-WEB/src/entities/Abrigo.ts
@@ -12,10 +12,22 @@ export class Abrigos{
 
     @Column({type: "text"})
     title_abrigo: string;
-
-    @Column({type: "text"})
-    rua_user: string;
     
     @Column("varchar", {length: 40})
     abrigo_type_user: string;
+
+    @Column({type: "text"})
+    bairro_abrigo: string;
+
+    @Column({type: "text"})
+    rua_abrigo: string;
+    
+    @Column({type: "text"})
+    complemento_abrigo: string;
+
+    @Column("varchar", { length: 15})
+    cep_abrigo: string;
+
+    @Column("varchar", { length: 7})
+    number_adress_abrigo: number;  
 }
